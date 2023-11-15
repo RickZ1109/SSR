8dc81257455ab1a7da26d9c0b6377f9aa2dae0bd
Ricardo Oliveira Silva <146988001+RickZ1109@users.noreply.github.com>
2023-11-15 20:19:17 -0300

    Update SssApiApplicationTests.java

--- src/test/java/com/sss/crm/SssApiApplicationTests.java
+++ src/test/java/com/sss/crm/SssApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.sss.crm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest /* <-- Marca a classe principal em um aplicativo Spring Boot, indicando que ela contém a configuração e inicialização do aplicativo.*/
 class SssApiApplicationTests {
 
 	@Test
