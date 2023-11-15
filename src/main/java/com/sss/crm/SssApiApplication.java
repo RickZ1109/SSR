62d8882f5f13e254b9d434bca73edf4c8326bde9
Ricardo Oliveira Silva <146988001+RickZ1109@users.noreply.github.com>
2023-11-15 20:25:12 -0300

    Update SssApiApplication.java

--- src/main/java/com/sss/crm/SssApiApplication.java
+++ src/main/java/com/sss/crm/SssApiApplication.java
@@ -3,7 +3,7 @@ package com.sss.crm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication /* <-- Marca a classe principal em um aplicativo Spring Boot, indicando que ela contém a configuração e inicialização do aplicativo.*/
 public class SssApiApplication {
 
 	public static void main(String[] args) {
