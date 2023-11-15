028749de5f8c3b2e77652a668b18ba2fa1f79168
Ricardo Oliveira Silva <146988001+RickZ1109@users.noreply.github.com>
2023-11-15 20:23:57 -0300

    Update ChamadoRepository.java

--- src/main/java/com/sss/crm/repository/ChamadoRepository.java
+++ src/main/java/com/sss/crm/repository/ChamadoRepository.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Repository;
 
 import com.sss.crm.model.Chamado;
 
-@Repository
+@Repository /* <-- Usada em classes que acessam e manipulam dados, geralmente servindo como um ponto de acesso ao banco de dados.
+Esta anotação indica ao Spring que a classe é um componente de persistência e permite a tradução de exceções específicas do banco de dados em exceções mais genéricas do Spring.*/
+
 public interface ChamadoRepository extends JpaRepository<Chamado, Long> {
 
 }
