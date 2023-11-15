a4ac41432dac2188b1826aeb056a1908d4c6b8f2
Ricardo Oliveira Silva <146988001+RickZ1109@users.noreply.github.com>
2023-11-15 20:21:12 -0300

    Update sssControler.java

--- src/main/java/com/sss/crm/controller/sssControler.java
+++ src/main/java/com/sss/crm/controller/sssControler.java
@@ -14,20 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sss.crm.model.Chamado;
 import com.sss.crm.repository.ChamadoRepository;
 
-@RestController
-@RequestMapping("/Chamado")
+@RestController /* <-- Controlador que lida com solicitações HTTP e retorna respostas no formato desejado (geralmente JSON).*/
+@RequestMapping("/Chamado") /* <-- Define a URL e o método HTTP que o método do controlador manipulará.*/
 public class sssControler {
 	
-	@Autowired
+	@Autowired /* <-- Anotação usada em Spring para realizar a injeção de dependência automaticamente,
+	conectando componentes do aplicativo sem a necessidade de criação manual de instâncias.*/
 	private ChamadoRepository chamadoRepository;
 
-	@GetMapping
+	@GetMapping /* <-- Anotação usada em um método de controle (marcado com @RestController)
+	para mapear solicitações HTTP GET para esse método, indicando a URL que acionará a execução do método.*/
 	public List<Chamado> listar() {
 		return chamadoRepository.findAll();
 	}
 	
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping /* <-- Mapea solicitações HTTP POST para esse método,
+	indicando a URL que acionará a execução do método. Essa anotação é usada para lidar com operações de criação ou envio de dados para o servidor.*/
+	
+	@ResponseStatus(HttpStatus.CREATED) /* <-- Usada para definir o código de status HTTP que um método de controle (marcado com @RestController)
+	deve retornar como resposta padrão. Permite personalizar o código de status da resposta HTTP para diferentes situações.*/
+	
 	public Chamado adicionar (@RequestBody Chamado chamado) {
 		return chamadoRepository.save(chamado);
 	}
