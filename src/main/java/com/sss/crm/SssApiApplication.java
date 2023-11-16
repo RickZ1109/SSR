6146a51b08c36833ebadca982b2e43da4c8ccdca
Ricardo Oliveira Silva <146988001+RickZ1109@users.noreply.github.com>
2023-11-15 21:47:08 -0300

    Update SssApiApplication.java

--- src/main/java/com/sss/crm/SssApiApplication.java
+++ src/main/java/com/sss/crm/SssApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SssApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SssApiApplication.class, args);
+		SpringApplication.run(SssApiApplication.class, args); /* <-- Inicia a aplicação em SpringBoot. */
 	}
 
 }
